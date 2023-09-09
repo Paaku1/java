652a3d41dcee2fc33569b00615e2d5b67e40997f
p1aaku <211001042@rajalakshmi.edu.in>
2023-09-09 19:04:31 +0530

    modifiers

---
+++ cons.java
@@ -0,0 +1,10 @@
+public class cons {
+    private String TAG = "cons";//private
+    public static void main(String[] args){
+        profile paaku = new profile("karthi",20);
+        System.out.println(paaku.name);
+        System.out.println(paaku.age);
+        cons tag = new cons();
+        System.out.println(tag.TAG);
+    }
+}
---
+++ profile.java
@@ -0,0 +1,12 @@
+public class profile {
+    public String name;
+    int age;
+
+    public profile(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public static void main(String[] args) {
+    }
+
+}
