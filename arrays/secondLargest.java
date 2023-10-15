70f194fe4305bc23c9a9c13b7f168c3c34941b88
p1aaku <211001042@rajalakshmi.edu.in>
2023-10-15 23:56:39 +0530

    ShiftZero

--- arrays/secondLargest.java
+++ arrays/secondLargest.java
@@ -3,7 +3,9 @@ package arrays;
 import java.util.Arrays;
 
 public class secondLargest {
-    int[] arr = {1,4,7,3,0,2,5,8};
-    Arrays.sort(arr);
-    System.out.println(arr[1]);
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 7, 3, 0, 2, 5, 8 };
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length - 2]);
+    }
 }
---
+++ arrays/shiftZeros.java
@@ -0,0 +1,10 @@
+package arrays;
+
+import java.util.*;
+
+public class shiftZeros {
+    public static void main(String[] args) {
+        int[] arr = { 5, 6, 0, 4, 6, 0, 9, 0, 8 };
+        System.out.println("Before Shifting: " + Arrays.toString(arr));
+    }
+}
