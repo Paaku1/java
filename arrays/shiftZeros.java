540088d8e214bc98f99a3051a3512775b3a899aa
p1aaku <211001042@rajalakshmi.edu.in>
2023-10-16 00:04:52 +0530

    update

--- arrays/shiftZeros.java
+++ arrays/shiftZeros.java
@@ -6,5 +6,24 @@ public class shiftZeros {
     public static void main(String[] args) {
         int[] arr = { 5, 6, 0, 4, 6, 0, 9, 0, 8 };
         System.out.println("Before Shifting: " + Arrays.toString(arr));
+        shiftZero(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void shiftZero(int[] nums) {
+        int i = 0;
+        int j = 0;
+        int t;
+        int n = nums.length;
+        for (i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+                j++;
+            }
+        }
     }
 }
